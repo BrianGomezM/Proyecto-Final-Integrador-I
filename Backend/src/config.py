f1a71c8569f43c44b4849c83712cb883a513e745
HectorDiiaz <hectorluis_d@hotmail.es>
2023-05-21 20:03:59 -0500

    Backend Update

--- Backend/src/config.py
+++ Backend/src/config.py
@@ -1,9 +1,9 @@
 class DevelopmentConfig(): #configuracion de desarrollo
     DEBUG = True
     #Datos del servidor MYSQL
-    MYSQL_HOST     = 'localhost'
-    MYSQL_USER     = 'root'
-    MYSQL_PASSWORD = ''
+    MYSQL_HOST     = '72.167.100.60'
+    MYSQL_USER     = 'Proyecto_Integrado_I_user'
+    MYSQL_PASSWORD = 'Proyecto_Integrado_I'
     MYSQL_DB       = 'Proyecto_Integrado_I'
     MYSQL_PORT     = 3306
 
