8dc40a66a77248feb30586e12a1facf51437d0aa
HectorDiiaz <hectorluis_d@hotmail.es>
2023-05-23 22:08:42 -0500

    Agrego criaturas mitologicas

--- Frontend/src/app/religion/mitologia/criaturas-mitologicas/criaturas-mitologicas.component.ts
+++ Frontend/src/app/religion/mitologia/criaturas-mitologicas/criaturas-mitologicas.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ConsumoServiciosService } from '../../../servicios/servicios-dioses/consumo-servicios.service';
+import { Dioses } from 'app/servicios/servicios-dioses/interface-dioses';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-criaturas-mitologicas',
   templateUrl: './criaturas-mitologicas.component.html',
   styleUrls: ['./criaturas-mitologicas.component.scss']
 })
 export class CriaturasMitologicasComponent implements OnInit {
+  tarjetasDuplicadas: any[] = [];
+  public listaDioses: Array<Dioses> = [];
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    private consumoServiciosService: ConsumoServiciosService
+
+  ) { }
 
   ngOnInit(): void {
+    this.consumoServiciosService.getAllGods().subscribe(
+      (dioses: Dioses[]) => {
+        this.listaDioses = dioses['dioses'];
+        
+      },
+      (error: any) => {
+        console.log('Error al obtener las construcciones:', error);
+        // Realiza acciones de manejo de errores aquí, como mostrar un mensaje al usuario o realizar alguna otra acción necesaria
+      }
+    );
   }
 
+  mostrarDetalles(dios:Dioses){
+    this.router.navigate(['/detalles-dios', dios.cod]);
+  }
 }
