af6210c2de832e9e0045cea171735a175f4834e2
HectorDiiaz <hectorluis_d@hotmail.es>
2023-05-27 08:12:43 -0500

    MV1 - TEST

--- Backend/src/controllers/controllerLogin.py
+++ Backend/src/controllers/controllerLogin.py
@@ -8,6 +8,7 @@ from flask import Flask
 # Crear el blueprint para el controlador de login
 login_app = Blueprint('login_app', __name__)
 
+CORS(login_app)
 # Método: POST
 # Datos de entrada: Se espera un JSON en el cuerpo de la solicitud que contenga el campo
 # nombreConstruccion con el nombre de la construcción que se desea registrar.
--- Backend/src/models/login.py
+++ Backend/src/models/login.py
@@ -98,5 +98,5 @@ class Login:
             'respuesta': respuesta,
             'recibido': 'OK'
         }
-
+        
         return response
