42069e73f5b029f4442d2c2b905592eb9a15a0d4
Eyder Suarez <eyder.suarez@correounivalle.edu.co>
2023-07-05 20:10:08 -0500

    otro cambio en backend

--- Backend/src/models/usuario.py
+++ Backend/src/models/usuario.py
@@ -29,36 +29,34 @@ ruta_imagenes = os.path.join(ruta_proyecto, "static", "imagenes")
 
 
 
-@staticmethod
-def crear_carpetas(ruta):
-    partes = ruta.split("/")
-    ruta_carpeta = ""
-    for parte in partes:
-        if parte == "":
-            continue
-        
-        ruta_carpeta += "/" + parte
-        
-        if not os.path.isdir(ruta_carpeta):
-            os.mkdir(ruta_carpeta)
-
-@staticmethod
-def obtener_datos_imagen(imagen, ruta):
-    encabezado, contenido = imagen.split(",", 1)
-    extension = encabezado.split(";")
-    extension = extension[0].split("/")
-    nombre_archivo = str(uuid.uuid4()) + "." + extension[-1]
-    ruta_archivo = os.path.join(ruta, nombre_archivo)
-    return [ruta_archivo, contenido]
-    
-@staticmethod
-def guardar_imagen(imagen, contenido):
-    ruta_archivo = os.path.join(app.static_folder, imagen)
-    with open(ruta_archivo, "wb") as archivo:
-        archivo.write(base64.b64decode(contenido))
 
 class Usuario:
-    
+    @staticmethod
+    def crear_carpetas(ruta):
+        partes = ruta.split("/")
+        ruta_carpeta = ""
+        for parte in partes:
+            if parte == "":
+                continue
+            ruta_carpeta += "/" + parte
+            if not os.path.isdir(ruta_carpeta):
+                os.mkdir(ruta_carpeta)
+
+    @staticmethod
+    def obtener_datos_imagen(imagen, ruta):
+        encabezado, contenido = imagen.split(",", 1)
+        extension = encabezado.split(";")
+        extension = extension[0].split("/")
+        nombre_archivo = str(uuid.uuid4()) + "." + extension[-1]
+        ruta_archivo = os.path.join(ruta, nombre_archivo)
+        return [ruta_archivo, contenido]
+
+    @staticmethod
+    def guardar_imagen(imagen, contenido):
+        ruta_archivo = os.path.join(app.static_folder, imagen)
+        with open(ruta_archivo, "wb") as archivo:
+            archivo.write(base64.b64decode(contenido))
+        
     def listar_usuarios():
         try:
             conn = mysql.connect()  # Establece la conexión a la base de datos
@@ -96,8 +94,8 @@ class Usuario:
             if 'urlAvatar' in usuario and usuario['urlAvatar']:
                 ruta_imagen = ruta_imagenes  # Ruta de almacenamiento de las imágenes
                 os.makedirs(ruta_imagen, exist_ok=True)  # Crea la estructura de carpetas si no existe
-                datos_imagen = obtener_datos_imagen(usuario['urlAvatar'], ruta_imagen)
-                guardar_imagen(datos_imagen[0], datos_imagen[1])
+                datos_imagen = Usuario.obtener_datos_imagen(usuario['urlAvatar'], ruta_imagen)
+                Usuario.guardar_imagen(datos_imagen[0], datos_imagen[1])
             
             #ruta_relativa = os.path.relpath(datos_imagen[0], ruta_proyecto)
             
@@ -109,7 +107,7 @@ class Usuario:
 
             # Insertar los datos del usuario en la base de datos, se guarda ruta_relativa para guardar la ubicación de la imagen relativa al proyecto
             if 'urlAvatar' in usuario and usuario['urlAvatar']:
-                ruta_relativa = os.path.relpath(datos_imagen[0], ruta_proyecto).replace('\\', '/').replace('static/', '')
+                ruta_relativa = os.path.relpath(datos_imagen[0], ruta_proyecto).replace('\\', '//').replace('static/', '')
             else: ruta_relativa = ''
             sql = "INSERT INTO usuario (nombre, apellido, telefono, correo, password, urlAvatar, sexo, estado) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"
             valores = (usuario['nombre'], usuario['apellido'], usuario['telefono'], usuario['correo'], usuario['password'], ruta_relativa, usuario['sexo'], usuario['estado'])
@@ -157,9 +155,9 @@ class Usuario:
                 if resultado and resultado[0] != comparador:
                     ruta_imagen = ruta_imagenes  # Ruta de almacenamiento de las imágenes
                     os.makedirs(ruta_imagen, exist_ok=True)  # Crea la estructura de carpetas si no existe
-                    datos_imagen = obtener_datos_imagen(usuario['urlAvatar'], ruta_imagen)
-                    guardar_imagen(datos_imagen[0], datos_imagen[1])
-                    ruta_relativa = os.path.relpath(datos_imagen[0], ruta_proyecto).replace('\\', '/').replace('static/', '')
+                    datos_imagen = Usuario.obtener_datos_imagen(usuario['urlAvatar'], ruta_imagen)
+                    Usuario.guardar_imagen(datos_imagen[0], datos_imagen[1])
+                    ruta_relativa = os.path.relpath(datos_imagen[0], ruta_proyecto).replace('\\', '//').replace('static/', '')
                 if resultado and resultado[0] == comparador:
                     ruta_relativa = resultado[0]
             else: ruta_relativa = ''
@@ -199,3 +197,37 @@ class Usuario:
         except Exception as ex:
             print(f"Error en actualizar_usuario: {str(ex)}")
             return {'mensaje': str(ex)}
+    # @staticmethod
+    # def modificar_usuario():
+    #     usuario = request.get_json()  # Obtener los datos enviados desde el frontend
+    #     resultado = Usuario.actualizar_usuario(usuario)  # Actualizar los datos en la base de datos
+
+    #     return jsonify(resultado)
+
+    # @staticmethod
+    # def actualizar_usuario(usuario):
+    #     try:
+    #         if 'id' not in usuario:
+    #             raise Exception("'id' no está presente en los datos del usuario")
+
+    #         conn = mysql.connect()  # Establecer la conexión a la base de datos
+    #         cursor = conn.cursor()
+
+    #         # Verificar si el correo electrónico ya está registrado para otro usuario
+    #         sql = "SELECT correo FROM usuario WHERE correo = %s AND id != %s"
+    #         cursor.execute(sql, (usuario['correo'], usuario['id']))
+    #         resultado = cursor.fetchone()
+    #         if resultado:
+    #             raise Exception("El correo electrónico ya está registrado para otro usuario")
+
+    #         # Actualizar los datos del usuario en la base de datos
+    #         sql = "UPDATE usuario SET nombre = %s, apellido = %s, telefono = %s, correo = %s, password = %s, urlAvatar = %s, sexo = %s, estado = %s WHERE id = %s"
+    #         valores = (usuario['nombre'], usuario['apellido'], usuario['telefono'], usuario['correo'], usuario['password'], usuario['urlAvatar'], usuario['sexo'], usuario['estado'], usuario['id'])
+    #         cursor.execute(sql, valores)
+
+    #         conn.commit()  # Confirmar los cambios en la base de datos
+    #         conn.close()  # Cerrar la conexión a la base de datos
+
+    #         return {'mensaje': 'El usuario se actualizó correctamente'}
+    #     except Exception as ex:
+    #         return {'mensaje': str(ex)}
--- Frontend/src/environments/environment.prod.ts
+++ Frontend/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  //baseUrl: 'http://127.0.0.1:5000/',
-  baseUrl: 'https://egyptianapi.onrender.com/',
+  baseUrl: 'http://127.0.0.1:5000/',
+  //baseUrl: 'https://egyptianapi.onrender.com/',
   // Resto de las propiedades
 };
